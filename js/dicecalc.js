a350be8fd80ad20db641a945739ddfea4fa76dd4
John Else <john.else@citrix.com>
2013-11-28 14:16:53 +0000

    Initial commit
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ js/dicecalc.js
@@ -0,0 +1,48 @@
+function parseDie(str) {
+    var i;
+    var result = [];
+    for (i = 0; i < str.length; i++) {
+        result.push(parseInt(str[i]));
+    }
+    return result;
+}
+
+function calcFrequencies(dice, dieIndex, subtotal, table) {
+    var i
+    // If we're on the last die, add the totals to the dictionary.
+    if (dieIndex == (dice.length - 1)) {
+        for (i = 0; i < dice[dieIndex].length; i++) {
+            var total = subtotal + dice[dieIndex][i];
+            if (table[total]) {
+                table[total] = table[total] + 1;
+            }
+            else {
+                table[total] = 1
+            }
+        }
+    }
+    // Otherwise, recurse down the tree for each face on this die.
+    else {
+        for (i = 0; i < dice[dieIndex].length; i++) {
+            calcFrequencies(dice, dieIndex + 1, subtotal + dice[dieIndex][i], table);
+        }
+    }
+}
+
+function doCalc() {
+    var dice = $("textarea#dice").val()
+        .split("\n")
+        .map(parseDie);
+    var frequencies = {};
+    var output = "";
+
+    calcFrequencies(dice, 0, 0, frequencies);
+    for (var total in frequencies) {
+        output += (total + "," + frequencies[total] + "\n");
+    }
+    $("textarea#results").val(output);
+}
+
+$(document).ready(function() {
+    $("button#docalc").click(doCalc);
+})
